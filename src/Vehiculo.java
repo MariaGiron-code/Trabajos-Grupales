8f9f09815415886554f9cfe7aa9e0e6649ccbfa1
POO <eduardo.ganchala@eon.edu.ec>
2025-04-24 09:31:34 -0500

    Creada la clase

---
+++ src/Vehiculo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Vehiculo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("PRUEBA EN GITHUB");
+        System.out.println("creada la clase");
+    }
+}
