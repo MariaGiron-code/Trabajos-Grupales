4c72fc2d9a326e0b0ff48660b74fe82c7b07206c
Odaliz Aracely <balsecaodaliz@gmail.com>
2025-04-24 09:40:22 -0500

    Odaliz Creo Atributos

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 public class Vehiculo {
+    //Atributos
+    String placa;
+    double precio;
+    String color;
+    String marca;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("PRUEBA EN GITHUB");
